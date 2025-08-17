e2a97367fabc92a65d0c1e5739cd5cd229b0cc8b
HaoHaoHan610 <kocoemail610@gmail.com>
2025-08-17 13:29:50 +0700

    foundation2

--- FlaskAPI/src/api.py
+++ FlaskAPI/src/api.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+api = Api(app)
 
 
 @app.route('/')
